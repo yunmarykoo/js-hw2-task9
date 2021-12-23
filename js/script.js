b3f7bd9bfe8ab09e77a7e53f90b83aa55c4574d5
Mary <yunmaryko@gmail.com>
2021-12-23 17:54:57 +0600

    changes

--- js/script.js
+++ js/script.js
@@ -1,14 +1,7 @@
-var guest = +prompt('Введите количество гостей для определения количества разрезов торта (включая и для Пети)');
+var guests = +prompt('Введите количество гостей') + 1;
 
-var evenGuest = ++guest;
-var oddGuest = ++guest;
-
-if (guest % 2 === 0){
-    alert ('Понадобится ' + evenGuest + ' разреров по радиусу')
-} else if (guest === 1){
-    alert('Понадобится 1 разрез по диаметру')
-} else if (guest === 3){
-    alert('Понадобится 2 разреза по диаметру')
-} else if (guest % 2 !== 0){
-    alert('Понадобится ' + oddGuest + ' разрезов по диаметру')
+if (guests % 2 === 0 ){
+    alert(guests / 2 + ' разрезов по диаметру');
+} else {
+    alert(guests + ' разрезов по радиусу') 
 }
